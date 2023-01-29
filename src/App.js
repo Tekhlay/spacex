d6cd7e269d7316caa977de61605ec8364a16ac49
Teklay <teklay@gmail.com>
2023-01-30 02:22:49 +0300

    Setup dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="Space-Travelers-container">
+      <div className="Space-Travelers-header">
+        <h1>Space Travelers Hub</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
